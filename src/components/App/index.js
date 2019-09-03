f137fbb2c0008185fcdbcd9018127cbde3f30cbc
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2019-09-03 09:32:34 +0200

    add Rote application

---
+++ src/components/App/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router,Route} from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+import Account from "../Account";
+import Admin from "../Admin";
+import Home from "../Home";
+import Landing from "../Landing";
+import Navigation from '../Navigation';
+import PasswordForget from "../PasswordForget";
+import SignIn from "../SignIn";
+import SignUp from "../SignUp";
+const App = () => (
+	<Router>
+		<Navigation />
+
+		<hr/>
+		<Route exact path={ROUTES.LANDING} component={Landing}/>
+		<Route path={ROUTES.SIGN_UP} component={SignUp}/>
+		<Route path={ROUTES.SIGN_IN} component={SignIn}/>
+		<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget}/>
+		<Route path={ROUTES.HOME} component={Home}/>
+		<Route path={ROUTES.ACCOUNT} component={Account}/>
+		<Route path={ROUTES.ADMIN} component={Admin}/>
+		<Route/>
+	</Router>
+);
+export default App;
