b81cceb86c7e385456b0eb0845e52ac2754085ce
Kamil Drążkiewicz <k.drazkiewicz@getprintbox.com>
2019-09-13 16:57:23 +0200

    PASSWORD RESET AND PASSWORD CHANGE WITH FIREBASE

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,11 +1,82 @@
-import React from 'react';
+import React, { Component }from 'react';
 
-const PasswordForget = () => {
-	return (
-		<div>
+import {Link} from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+import  { withFirebase } from "../Firebase";
 
-		</div>
-	);
+const PasswordForgetPage = () => (
+	<div>
+		<h1>Password Forget</h1>
+		<PasswordForgetForm/>
+	</div>
+);
+
+const INITIAL_STATE = {
+	email: '' ,
+	error: ''
 };
 
-export default PasswordForget;
+class PasswordForgetBase extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = { ...INITIAL_STATE }
+	}
+
+
+	onSubmit = event => {
+		const { email } = this.state
+
+		this.props.firebase
+			.doPasswordReset(email)
+			.then(()=>{
+				this.setState({ ...INITIAL_STATE })
+			})
+			.catch(error => {
+				this.setState({error})
+			});
+
+		event.preventDefault();
+
+	};
+
+	onChange = event => {
+		this.setState({[event.target.name]: event.target.value})
+	};
+
+
+	render (){
+		const {email, error} = this.state;
+
+		const isInValid = email === '';
+		return(
+			<form onSubmit={this.onSubmit}>
+				<input
+					name="email"
+					type="text"
+					value={email}
+					onChange={this.onChange}
+					placeholder="Email Addres"
+				/>
+				<button disabled={isInValid} type="submit">
+					Reset My Password
+				</button>
+				{error&& <p>{error.message}</p>}
+			</form>
+		);
+	}
+
+}
+
+
+const PasswordForgetLink = () => (
+	<p>
+		<Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
+	</p>
+);
+
+export default PasswordForgetPage;
+
+const PasswordForgetForm = withFirebase(PasswordForgetBase);
+
+export {PasswordForgetForm, PasswordForgetLink}
