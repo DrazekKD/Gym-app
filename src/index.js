ff9a74a7c782d8950fb014fccd40860a767c05e1
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2019-09-17 14:43:54 +0200

    Redux setup session and user

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux'
+import store from './store'
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import Firebase, {FirebaseContext} from "./components/Firebase";
 ReactDOM.render(
+	<Provider store={store}>
 	<FirebaseContext.Provider value={new Firebase()}>
 		<App />
-	</FirebaseContext.Provider>,
+	</FirebaseContext.Provider>
+	</Provider>,
 	document.getElementById('root'),
 );
 serviceWorker.unregister();
